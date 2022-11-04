399ec79c5072ff7e635fd20a319018f3c5966e04
Marcos Keller <marcoskeller@hotmail.com>
2022-11-03 21:24:15 -0300

    Criacao do projeto de padroes e estilizacao de paginas Web

---
+++ Padroes_Estilizacao_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Padroes_Estilizacao_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
