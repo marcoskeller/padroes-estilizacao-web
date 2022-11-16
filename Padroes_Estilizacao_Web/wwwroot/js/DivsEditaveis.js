74c66a64dc85dae0de5595c56ee6b8962d6afda6
Marcos Keller <marcoskeller@hotmail.com>
2022-11-16 00:32:34 -0300

    Implementacao Divs Eitaveis Parte-2

--- Padroes_Estilizacao_Web/wwwroot/js/DivsEditaveis.js
+++ Padroes_Estilizacao_Web/wwwroot/js/DivsEditaveis.js
@@ -1,28 +1,48 @@
 ﻿
 
-$('.divCustomizavel').draggable(
-    {
-        containment: '#bord',
-    }
-);
+var contaIndex = 1;
+
+var elementos = document.getElementsByClassName('divcustomizavel');
+var div = Array.prototype.filter.call(elementos, function (div) {
+
+    $("#" + div.id).draggable({
+        containment: "#bord",
+    });
 
 
-$('.resizeButton').draggable(
-    {
-        containment: '#bord',
+    $("#A" + div.id).draggable({
+        containment: "#bord",
         drag: function () {
-
-            $('.divCustomizavel').height($('.resizeButton').position().top - 8);
-            $('.divCustomizavel').width($('.resizeButton').position().left - 8);
+            $("#" + div.id).height($("#A" + div.id).position().top - 8);
+            $("#" + div.id).width($("#A" + div.id).position().left - 8);
+            $("#" + div.id).css("z-index", "1000");
         }
-    }
-);
+    });
 
-
+});
 
 
 $(function () {
 
-    $('.divCustomizavel').css("width", "100px");
-});
+    var elementos = document.getElementsByClassName('divcustomizavel');
 
+    var posicaoDiv = 120;
+
+    for (var i = 0; i < elementos.length; i++) {
+
+        $("#" + elementos[i].id).click(function () {
+            $("#" + this.id).css("z-index", "" + contaIndex + "" + "000");
+            contaIndex++;
+        });
+
+        $("#" + elementos[i].id).css("width", '250px');
+        $("#" + elementos[i].id).css("height", '100px');
+
+    }
+
+    $("#div2").css("top", '122px');
+    $("#div3").css("top", '228px');
+    $("#div4").css("top", '336px');
+    $("#div5").css("top", '536px');
+
+});
